c622f34c4d8cc4a3ca8c230a2f7d333ae4006aad
natanong <natanongchaitham@gmail.com>
2018-02-25 13:48:02 +0700

    Show Dashboard

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,69 @@
 package sample;
 
-public class Controller {
-}
+public class Controller implements Initializable {
+    private loginModel loginmodel = new loginModel();
+
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private Label loginStatus;
+
+    @FXML
+    private Label dbStatus;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        if (this.loginmodel.isDatabaseConnection()) {
+            this.dbStatus.setText("Connected to DB.");
+        } else {
+            this.dbStatus.setText("Not Connect to DB.");
+        }
+    }//initialize
+
+    @FXML
+    public void Login(ActionEvent event){
+        try {
+            if (this.loginmodel.isLogin(username.getText(), password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+                adminDashboard();
+
+            } else {
+                loginStatus.setText("Your username or password is invalid.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }//Login
+
+    private void adminDashboard() {
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = (Pane) adminLoader.load(
+                    getClass().getResource("/admin/adminDashboard.fxml").openStream());
+            adminController adminController = adminLoader.getController();
+            Scene scene = new Scene(adminRoot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashboard");
+            adminStage.setResizable(false);
+            adminStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }//adminDashboard
+
+
+}//class
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,10 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("School Management System");
+        primaryStage.setScene(new Scene(root, 600, 390));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -20,4 +21,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+}//class
