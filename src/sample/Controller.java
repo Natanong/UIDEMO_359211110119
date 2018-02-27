e4a5887f6fca5c3147beb9ba968860bdc6265c3e
natanong <natanongchaitham@gmail.com>
2018-02-27 12:18:28 +0700

    Logout Stage

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,24 @@
 package sample;
 
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     private loginModel loginmodel = new loginModel();
-
-
-
-
+    private JEditorPane dbStatus;
+    private JFXPanel btnLogin;
 
 
     @Override
@@ -30,6 +34,8 @@ public class Controller implements Initializable {
     @FXML
     public void Login(ActionEvent event){
         try {
+            JEditorPane username;
+            AbstractButton password;
             if (this.loginmodel.isLogin(username.getText(), password.getText())) {
                 Stage stage;
                 stage = (Stage) this.btnLogin.getScene().getWindow();
@@ -37,6 +43,7 @@ public class Controller implements Initializable {
                 adminDashboard();
 
             } else {
+                JEditorPane loginStatus;
                 loginStatus.setText("Your username or password is invalid.");
             }
         } catch (Exception e) {
@@ -52,7 +59,8 @@ public class Controller implements Initializable {
             FXMLLoader adminLoader = new FXMLLoader();
             Pane adminRoot = (Pane) adminLoader.load(
                     getClass().getResource("/admin/adminDashboard.fxml").openStream());
-            adminController adminController = adminLoader.getController();
+            adminController adminController;
+            adminController = adminLoader.getController();
             Scene scene = new Scene(adminRoot);
             adminStage.setScene(scene);
             adminStage.setTitle("Admin Dashboard");
